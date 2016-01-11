19a26b22566acb9ef038c09af568b93ef84ee93f
tamaz bagdavadze <tamazbagdavadze@gmail.com>
2016-01-11 09:01:00 -0800

    1.0 beta

---
+++ Windows service sample/Windows service sample/MyInstaller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace Windows_service_sample
+{
+    [RunInstaller(true)]
+    public partial class MyInstaller : Installer
+    {
+        public MyInstaller()
+        {
+            InitializeComponent();
+
+            Installers.Add(new ServiceProcessInstaller
+            {
+                Account = ServiceAccount.LocalSystem
+            });
+
+            Installers.Add(new ServiceInstaller
+            {
+                ServiceName = "my service",
+                Description = "my service!!!",
+                StartType = ServiceStartMode.Automatic
+            });
+        }
+    }
+}
--- Windows service sample/Windows service sample/Service1.cs
+++ Windows service sample/Windows service sample/Service1.cs
@@ -3,26 +3,54 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
+
 
 namespace Windows_service_sample
 {
     public partial class Service1 : ServiceBase
     {
+
+        private readonly Timer timer;
+
         public Service1()
         {
             InitializeComponent();
+            timer = new Timer(500);
+            timer.Elapsed += Timer_Elapsed;
+        }
+
+        private void Timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            var path = @"d:\\1.txt";
+            var text = "some text";
+
+            File.WriteAllText(path, text);
         }
 
         protected override void OnStart(string[] args)
         {
+            timer.Start();
         }
 
         protected override void OnStop()
         {
+            timer.Stop();
+        }
+
+        protected override void OnPause()
+        {
+            this.OnStop();
+        }
+
+        protected override void OnContinue()
+        {
+            this.OnStart(null);
         }
     }
 }
